c325c4230cbb016daab0c40163b5a05f042af871
unknown <ikmssaid24@lhusurbil.eus>
2025-11-04 20:30:45 +0100

    documentacion y .exe hechos

--- p4.py
+++ p4.py
@@ -4,7 +4,7 @@ import datetime, threading, requests, json, subprocess, time, os
 
 #globales
 ficheros = []
-url="http://api-grupo3.duckdns.org/"
+url="https://api-grupo3.duckdns.org/"
 intervalo_api=0.1
 intervalo_ficheros=2
 carpeta="ficheros/"
@@ -16,7 +16,7 @@ def getApi(tabla):
         "user":"ikmssaid24@lhusurbil.eus",
         "password":"usuario"
     }
-    a= requests.post(url+"login", json=d)#realizo login
+    a= requests.post(url+"login", json=d)#realizo login pasandole usuario y contrasenia
     v=a.cookies#guardo la coockie
     a= requests.get(url+"getDatosFiltro/"+tabla,cookies=v)# hago el get pasandole la cookie
     return json.dumps(a.json(), indent=4)
@@ -41,13 +41,13 @@ def monitorizar_ficheros():#funcion para que acada x tiempo revise si existen fi
             if fichero.endswith(".json") and fichero not in ficheros:
                 ficheros.append(fichero)
                 print(f"fichero detectado: {fichero}")
-        time.sleep(intervalo_ficheros)  # revisa cada x segundos
+        time.sleep(intervalo_ficheros)  # para x segundos y vuelve a ejecutar
 
-def mostrar_ficheros(numeros=False):
+def mostrar_ficheros(numeros=False):#funcion que se encarga de mostrar por pantalla todos los ficheros
     print(f""".
 └── {carpeta}""")
     for i in range(len(ficheros)):
-        if numeros:
+        if numeros:# lo separo por la variable numeros para mostrar el numero correspondiente al fichero o no
             if i<len(ficheros)-1:
                 print(f"\t├──[{i}]", ficheros[i])
             else:
@@ -58,48 +58,48 @@ def mostrar_ficheros(numeros=False):
             else:
                 print( "\t└──", ficheros[i], "\n")
 
-def aniadir():
+def aniadir():#funcion para aniadir nuevos ficheros al directorio
     mostrar_ficheros()
     nombre=input("\nIntroduzca el nombre del nuevo fichero:")
     try:
-        with open(carpeta+nombre, "x"):
+        with open(carpeta+nombre, "x"):#aqui con x para que lance error si ya existe el fichero
             ficheros.append(nombre)
             print(f"El fichero {nombre} ha sido creado correctamente")
     except:
         print("Error, fallo en la operacion, asegurese de que no haya un fichero con el mismo nombre...")
 
-def editar():
+def editar():#funcion para editar el contenido de un fichero
     mostrar_ficheros(True)
     try:
-        i=int(input("\nIntroduzca el numero del fichero a editar:"))
+        i=int(input("\nIntroduzca el numero del fichero a editar:"))#recojo la posicion relacionada con el fichero a editar
         if i<0:
             print("Error, valor mal introducido...")
             return
-        fich=carpeta+ficheros[i]
+        fich=carpeta+ficheros[i]#asigno la ruta desde el directorio actual y me aseguro de que existe
         if not os.path.exists(fich):
             print("El fichero no existe.")
             return
-        subprocess.run(["notepad.exe", fich])
+        subprocess.run(["notepad.exe", fich])#lo ejecuto con el bloc de notas
     except:
         print("Error, no se ha podido realizar la operacion...")
 
-def eliminar():
+def eliminar():#funcion para eliminar un fichero
     try:
         mostrar_ficheros(True)
         i=int(input("\nIntroduzca el fichero a eliminar:"))
         if i<0:
-            print("Error, valor mal introducido...")
+            print("Error, valor mal introducido...")#control de errores
             return
         fich=carpeta+ficheros[i]
         if os.path.exists(fich):
             os.remove(fich)
-            print(f"Fichero \"{ficheros.pop(i)}\" eliminado correctamente...")
+            print(f"Fichero \"{ficheros.pop(i)}\" eliminado correctamente...")#el pop para eliminarlo y poder mostrar despues la correcta eliminacion de fichero
         else:
             print("Error, el fichero no existe...")
     except:
         print("Error la operacion no se ha podido completar...")
 
-def menu_ficheros():
+def menu_ficheros():#funcion para navegacion entre las opciones de la opcion 1-ficheros
     while True:
         try:
             opc=int(input("""\nIntroduzca una opcion:
@@ -124,9 +124,9 @@ def menu_ficheros():
             continue
 
 
-def configuracion():
+def configuracion():#funcion que cambia la configuracion cambiando las variables globales
     global url, intervalo_api, intervalo_ficheros, carpeta, tablas
-    while True:
+    while True:#bucle para elegir cambiar algo o volver atras
         print(f"""
               ATENCION NO NOS HACEMOS CARGO DEL MAL USO QUE SE LE PUEDA DAR A LA APLICACION 
               Si usted cambia algun parametro y la aplicacion deja de funcionar reiniciela y
@@ -142,7 +142,7 @@ def configuracion():
         try:
             opc=int(input("Seleccione una opcion:"))
             if opc==0:
-                    try:
+                    try:#basicamente asigna el input a la global y ya
                         i=input("""
                               ¡Atencion! esta a punto de cambiar parametros importantes para el buen funcionamiento de la aplicacion
                               asegurese de que sus datos sean correctos antes de cambiarlos.
@@ -153,7 +153,7 @@ def configuracion():
                     except:
                         print("Los datos han sido mal introducidos, aviso, los cambios no se van a efectuar")
             elif opc==1:
-                    try:
+                    try:#y aqui necesito pasarlo a int
                         i=int(input("""
                               ¡Atencion! esta a punto de cambiar parametros importantes para el buen funcionamiento de la aplicacion
                               asegurese de que sus datos sean correctos antes de cambiarlos.
@@ -203,15 +203,15 @@ def configuracion():
                 continue
 
 
-def ping():
+def ping():#funcion que realiza un ping a un dominio y no url
     try:
-        url_ping=input("Introduzca la url a la que quiere hacer el ping:")
-        salida = subprocess.run(["ping", "-n", "2", url_ping ], capture_output=True, text=True)
+        ping=input("Introduzca el dominio al que quiere hacer el ping(Sin 'http(s)://'):")
+        salida = subprocess.run(["ping", "-n", "2", ping ], capture_output=True, text=True)#ejecuto el ping con parametros -n y 2 para solo dos requests
         print( salida.stdout)
     except:
         print("Error no se ha podido realizar la operacion...")
 
-def mostrar_menu():
+def mostrar_menu():#funcion que muestra el menu y controla la opciones ejecutando una funcion segun la opcion introducida
     while True:
         try:
             opc=int(input("""
@@ -236,9 +236,9 @@ def mostrar_menu():
             continue
 
 
-if __name__=="__main__":
-    threading.Thread(target=generarficheros, daemon=True).start()
+if __name__=="__main__":#aqui empieza la aplcacion
+    threading.Thread(target=generarficheros, daemon=True).start()#ejecuto las funciones generar... y monitorizar... en un thread para que se ejecute en segundo plano y la aplicacion siga funcionando
     threading.Thread(target=monitorizar_ficheros, daemon=True).start()
-    time.sleep(2)
-    mostrar_menu()
+    time.sleep(2)#le pongo un sleep para que le de tiempo a monitorizar a detectar los ficheros y no rompa el menu con los prints
+    mostrar_menu()#inicio la funcion principal
         
